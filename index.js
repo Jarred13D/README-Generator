d8f5f658f8659644f62c7ca39c3afbc9927cf895
Goose_tG <jarredhampton@hotmail.com>
2025-02-04 23:32:09 -0500

    fixed format bug

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const questions = [];
 
         -----------------------------------------
 
-        ## [Installation])
+        ## [Installation]
         ${answers.installation}
 
         -----------------------------------------
